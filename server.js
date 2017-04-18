0f95385a9531238d2572a916cffd7270e7a1e78d
Austin D'Amico <adamico2412@gmail.com>
2017-04-18 15:36:53 +0000

    basic timestamp functionality implemented in server.js

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
+var strftime = require('strftime');
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.end('Hello World!');
+function getTimes(time) {
+    var unixTime, naturalTime;
+    
+    if (isNaN(time.getTime())) {
+        unixTime = null;
+        naturalTime = null;
+    } else {
+        unixTime = time.getTime();
+        naturalTime = strftime('%B %d, %Y', time);
+    }
+    
+    return {
+        unix: unixTime,
+		natural: naturalTime
+	};
+}
+
+app.get('/:date', function(req, res) {
+
+  var date = req.params.date;
+
+  date = Number(date) ? new Date(Number(date)) : new Date(date);
+  
+  res.end(JSON.stringify(getTimes(date)));
 });
 
 app.listen(8080, function(){
